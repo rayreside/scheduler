e9e825e384e05b157b5a5b23839756b54048428a
unknown <scrambled.humor@gmail.com>
2021-06-05 18:37:21 -0400

    minor styling additions

--- src/Shift.js
+++ src/Shift.js
@@ -1,23 +1,46 @@
 import React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
+import { DataGrid, GridColDef } from '@material-ui/data-grid';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import './Shift.css';
 
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      'fot-seurat-pron',
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ],
+    fontWeight: 600,
+    fontSize: 20
+  },
+});
+
 // define which columns you have in your data
-const columns = [
-    { field: 'id', headerName: 'Order', hide: 'true' },
-    { field: 'TASK', headerName: 'Task', width: 300 },
-    { field: 'ASSIGNED', headerName: 'Assigned', width: 300 }
+const columns: GridColDef[] = [
+    { field: 'id', headerName: 'Order', width: 130 },
+    { field: 'TASK', headerName: 'Task', width: 300, sortable: 'false' },
+    { field: 'ASSIGNED', headerName: 'Assigned', width: 300, sortable: 'false' }
   ];
 
 const Sched = ({ tasklist }) => {
 
     return(
-                <DataGrid autoHeight rows={tasklist} columns={columns} sortModel={[
-                    {
-                      field: 'id',
-                      sort: 'asc',
-                    },
-                  ]} />
+      <ThemeProvider theme={theme}>
+        <DataGrid disableColumnMenu={true} autoHeight rows={tasklist} columns={columns} sortModel={[
+            {
+              field: 'id',
+              sort: 'asc',
+            },
+          ]} />
+      </ThemeProvider>
     )
 }
 
